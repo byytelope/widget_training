a8014450a2d93f18978db15c74a8a1907b745180
Mohamed Shadhaan <shadhanm@gmail.com>
2025-04-29 02:55:09 +0800

    Init

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import "package:flutter/material.dart";
+import "package:widget_training/screens/home_screen.dart";
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Widget Training",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
